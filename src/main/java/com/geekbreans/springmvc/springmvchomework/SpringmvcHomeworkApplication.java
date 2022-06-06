01e02e739fe9f6cefeecf3133c524be77347e2a7
ZholudV <goldexzholud@gmail.com>
2022-06-06 11:34:19 +0500

    springMVC домашняя работа

---
+++ src/main/java/com/geekbreans/springmvc/springmvchomework/SpringmvcHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.geekbreans.springmvc.springmvchomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmvcHomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geekbreans/springmvc/springmvchomework/SpringmvcHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekbreans.springmvc.springmvchomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
